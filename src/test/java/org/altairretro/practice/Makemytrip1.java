2712e407691483b3a515763808d4111757acafc1
PREM KUMAR R <PREM KUMAR R@DESKTOP-VUHVFPU>
2022-09-21 12:20:52 +0530

    axaX

--- src/test/java/org/altairretro/practice/Makemytrip1.java
+++ src/test/java/org/altairretro/practice/Makemytrip1.java
@@ -11,7 +11,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Makemytrip1 {
 
 	public static void main(String[] args) {
-		//changes done by engg-2
+		//changes done by engg-1
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
